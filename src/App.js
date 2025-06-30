5b8c0df89aa90faf3a1aa263b70dad16f7680a59
Tycheon18 <joong0818@gmail.com>
2025-07-01 00:28:21 +0900

    Refactor TodoList state management from useState to useReducer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import { useState, useRef } from "react"
+import { useReducer, useRef } from "react"
 import './App.css';
 import Header from "./component/Header";
 import TodoEditor from './component/TodoEditor';
 import TodoList from './component/TodoList';
+import TestComp from './component/TestComp';
 
 const mockTodo = [
   {
@@ -25,35 +26,64 @@ const mockTodo = [
   },
 ]
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "CREATE": {
+      return [action.newItem, ...state];
+    }
+    case "UPDATE": {
+      return state.map((it) =>
+        it.id === action.targetId
+          ? {
+            ...it,
+            isDone: !it.isDone,
+          }
+          :it
+      );
+    }
+    case "DELETE": {
+      return state.filter((it) => it.id !== action.targetId);
+    }
+    default:
+      return state;
+  }
+}
+
 function App() {
   const idRef = useRef(3);
-  const [todo, setTodo] = useState(mockTodo);
+  const [todo, dispatch] = useReducer(reducer, mockTodo);
 
   const onCreate = (content) => {
-    const newItem = {
-      id: idRef.Current,
-      content,
-      isDone: false,
-      createdDate: new Date().getTime(),
-    };
-    setTodo([newItem, ...todo])
+    dispatch({
+      type: "CREATE",
+      newItem: {
+        id: idRef.current,
+        content,
+        isDone: false,
+        createdDate: new Date().getTime(),
+      },
+    })
+    
     idRef.current += 1;
-  }
+  };
 
   const onUpdate = (targetId) => {
-    setTodo(
-      todo.map((it) => 
-        it.id === targetId ? { ...it, isDone: !it.isDone } : it    
-      )
-    )
-  }
+    dispatch({
+      type: "UPDATE",
+      targetId,
+    });
+  };
 
   const onDelete = (targetId) => {
-    setTodo(todo.filter((it) => it.id !== targetId));
-  }
+    dispatch({
+      type: "DELETE",
+      targetId,
+    });
+  };
   
   return (
     <div className="App">
+      <TestComp />
       <Header />
       <TodoEditor onCreate={onCreate}/>
       <TodoList todo={todo} onUpdate={onUpdate} onDelete={onDelete} />
